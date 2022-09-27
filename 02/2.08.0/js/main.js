ce944135e435238343b6e14e7019d427d748ca90
maxmay94 <maxmay94@gmail.com>
2022-09-27 16:03:32 -0400

    update building height chart

--- 02/2.08.0/js/main.js
+++ 02/2.08.0/js/main.js
@@ -10,16 +10,19 @@ const data = d3.json('data/buildings.json').then(data => {
   })
   
   const svg = d3.select('#chart-area').append('svg')
-  .attr('width', 800)
+  .attr('width', 400)
   .attr('height', 800)
   
   const rects = svg.selectAll('rect')
   .data(data)
+
   
   rects.enter().append('rect')
-  .attr('x', (d, i) => (i*50 + 50))
+  .attr('x', (d, i) => (i*60))
   .attr('y', 200)
   .attr('height', (d) => d.height)
   .attr('width', '30')
   .attr('fill', 'gray')
+  .append('text')
+  .text(d.name)
 })
