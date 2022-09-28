3d11176d1d03b1dff0c9a495ebc47f3bdef32b00
maxmay94 <maxmay94@gmail.com>
2022-09-28 11:18:48 -0400

    add files

--- 03/3.13.0/js/main.js
+++ 03/3.13.0/js/main.js
@@ -27,56 +27,63 @@ d3.csv('data/revenues.csv').then(data => {
     d.revenue = Number(d.revenue)
     d.profit = Number(d.profit)
   })
-
   // SCALES
   const y = d3.scaleLinear()
-    .domain([0, d3.max(data, d => d.revenue)])
     .range([HEIGHT, 0])
   const x = d3.scaleBand()
-    .domain(data.map(d => d.month))
     .range([0, WIDTH])
     .paddingInner(0.2)
     .paddingOuter(0.2)
 
-  // DRAW AXES
-  const xAxis = d3.axisBottom(x)
-  g.append('g')
-    .attr('class', 'x axis')
-    .attr('transform', `translate(0, ${HEIGHT})`)
-    .call(xAxis)
+  d3.interval(() => {
+    update(data)
+  }, 1000)
 
-  const yAxis = d3.axisLeft(y)
-    .ticks(5)
-    .tickFormat(d => "$" + d )
-  g.append('g')
-    .attr('class', 'y axis')
-    .call(yAxis)
+  function update(data) {
 
-  // LABEL AXES
-  // X Axis
-  g.append('text')
-    .attr('x', WIDTH / 2)
-    .attr('y', HEIGHT + 40)
-    .attr('font-size', '20px')
-    .attr('text-anchor', 'middle')
-    .text('Month')
-  // Y Axis
-  g.append('text')
-    .attr('x', -(HEIGHT / 2))
-    .attr('y', -60)
-    .attr('font-size', '20px')
-    .attr('text-anchor', 'middle')
-    .attr('transform', 'rotate(-90)')
-    .text('Revenue')
-
-  // DRAW RECTS
-  const rects = g.selectAll('rect')
-    .data(data)
-  rects.enter().append('rect')
-    .attr('x', d => x(d.month))
-    .attr('y', d => y(d.revenue))
-    .attr('width', x.bandwidth)
-    .attr('height', d => HEIGHT - y(d.revenue))
-    .attr('fill', 'green')
-
+    x.domain(data.map(d => d.month))
+    y.domain([0, d3.max(data, d => d.revenue)])
+  
+    // DRAW DATA
+    // const rects = g.selectAll('rect')
+    //   .data(data)
+    // rects.enter().append('rect')
+    //   .attr('x', d => x(d.month))
+    //   .attr('y', d => y(d.revenue))
+    //   .attr('width', x.bandwidth)
+    //   .attr('height', d => HEIGHT - y(d.revenue))
+    //   .attr('fill', 'green')
+  
+    // DRAW AXES
+    const xAxis = d3.axisBottom(x)
+    g.append('g')
+      .attr('class', 'x axis')
+      .attr('transform', `translate(0, ${HEIGHT})`)
+      .call(xAxis)
+  
+    const yAxis = d3.axisLeft(y)
+      .ticks(5)
+      .tickFormat(d => "$" + d )
+    g.append('g')
+      .attr('class', 'y axis')
+      .call(yAxis)
+  
+    // LABEL AXES
+    // X Axis
+    g.append('text')
+      .attr('class', 'x axis')
+      .attr('x', WIDTH / 2)
+      .attr('y', HEIGHT + 40)
+      .attr('font-size', '20px')
+      .attr('text-anchor', 'middle')
+      .text('Month')
+    // Y Axis
+    g.append('text')
+      .attr('x', -(HEIGHT / 2))
+      .attr('y', -60)
+      .attr('font-size', '20px')
+      .attr('text-anchor', 'middle')
+      .attr('transform', 'rotate(-90)')
+      .text('Revenue')
+  }
 })
