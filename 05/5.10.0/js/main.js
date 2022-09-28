9b8d89fc1b93193b72ac65c7451a5b6b57fc275c
maxmay94 <maxmay94@gmail.com>
2022-09-28 16:04:07 -0400

    reformat data

--- 05/5.10.0/js/main.js
+++ 05/5.10.0/js/main.js
@@ -7,7 +7,9 @@
 const MARGIN = { LEFT: 100, RIGHT: 10, TOP: 10, BOTTOM: 100 }
 const WIDTH = 600 - MARGIN.LEFT - MARGIN.RIGHT
 const HEIGHT = 400 - MARGIN.TOP - MARGIN.BOTTOM
+let year = 1800
 
+// SELECT char area and append SVG then append main Group
 const svg = d3.select("#chart-area").append("svg")
   .attr("width", WIDTH + MARGIN.LEFT + MARGIN.RIGHT)
   .attr("height", HEIGHT + MARGIN.TOP + MARGIN.BOTTOM)
@@ -15,11 +17,29 @@ const svg = d3.select("#chart-area").append("svg")
 const g = svg.append("g")
   .attr("transform", `translate(${MARGIN.LEFT}, ${MARGIN.TOP})`)
 
+const x = d3.scaleLog()
+	.domain([0, WIDTH])
+	.base(10)
+
+
+// READ DATA
 d3.json("data/data.json").then(data =>{
-	let newData = new Map()
-	data.forEach((d, i) => {
-		newData.set(Number(d.year), d.countries.filter(c => c.life_exp))
+
+	let newData = data.map(d => {
+		let maxPop = 0
+		let maxLifeExp = 0
+		d.countries.forEach(c => {
+			if(c.life_exp > maxLifeExp) maxLifeExp = c.life_exp
+			if(c.population > maxPop) maxPop = c.population
+		})
+		d.maxPop = maxPop
+		d.maxLifeExp = maxLifeExp
+		return d
 	})
+	
+
+	
+	console.log(newData)
 
 	d3.interval(() => {
 		update(newData)
